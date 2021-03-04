3c3e097d9b8ea7a7e0f840cbcb45cf95baee2093
Andrewmurashko <anamay94.am@gmail.com>
2021-03-04 18:58:16 +0300

    fix:settings rules

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import { Menu, Game, Settings, Tutorial, Records } from './pages';
 import { Footer } from './components';
 function App() {
   const [isDarkTheme, setIsDarkTheme] = React.useState(true);
-  
+  const [movesRule, setMovesRule] = React.useState(true);
+  const [repititionRule, setRepititionRule] = React.useState(true);
   React.useEffect(() => {
     if(localStorage.getItem('Settings')) {
       setIsDarkTheme(JSON.parse(localStorage.getItem('Settings')).isDarkTheme)
+      setMovesRule(JSON.parse(localStorage.getItem('Settings')).movesRule)
+      setRepititionRule(JSON.parse(localStorage.getItem('Settings')).repititionRule)
     }
   }, [])
-  const [movesRule, setMovesRule] = React.useState(true);
-  const [repititionRule, setRepititionRule] = React.useState(true);
+ 
   return (
     <div className={isDarkTheme ? `theme_dark wrapper` : `theme_colored wrapper`}>
       <Route exact path="/" component={Menu}></Route>
